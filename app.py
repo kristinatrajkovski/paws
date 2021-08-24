de3e389a41560d30183f4c218ebd6e2d1a1d7aa4
RedFloFox <redfoxmma@gmail.com>
2021-08-24 19:15:10 -0400

    created messages page

--- app.py
+++ app.py
@@ -5,25 +5,29 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('home.html', title="Home Page")
+    return render_template('home.html')
 
 @app.route("/profile/<string:user_id>")
 def profile(user_id):
     person=users[user_id]
-    return render_template('profile.html', title="Profile", person=person)
+    return render_template('profile.html', person=person)
 
 @app.route("/missing")
 def missing():
-    return render_template('missing.html', title="Missing Dog")
+    return render_template('missing.html')
 
 @app.route("/found")
 def found():
-    return render_template('found.html', title="Report Found Dog")
+    return render_template('found.html')
 
 @app.route("/adoption")
 def adoption():
-    return render_template('putfora.html', title="Put Dog Up for Adoption")
+    return render_template('putfora.html')
 
 @app.route("/adopt")
 def adopt():
-    return render_template('adopt.html', title="Put Dog Up for Adoption")
+    return render_template('adopt.html')
+
+@app.route("/messages")
+def messages():
+    return render_template('messages.html')
