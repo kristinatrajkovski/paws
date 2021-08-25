98cd6aeb45e1f2284929708a222c60590159e8ca
Kristina <trajkovskikristina@gmail.com>
2021-08-25 21:55:24 +0300

    improved lower navbar, titles, message icon in upper menu

--- app.py
+++ app.py
@@ -8,29 +8,29 @@ served no purpose """
 
 @app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', title= "Home")
 
 @app.route("/profile/<string:user_id>")
 def profile(user_id):
     person=users[user_id]
-    return render_template('profile.html', person=person)
+    return render_template('profile.html', person=person, title=person['Name'])
 
 @app.route("/missing")
 def missing():
-    return render_template('missing.html')
+    return render_template('missing.html', title= "Search for Missing Animal")
 
 @app.route("/found")
 def found():
-    return render_template('found.html')
+    return render_template('found.html', title= "Report a Found Animal")
 
-@app.route("/adoption") #the fact that this is adoption and the 
-def adoption():         #one below is adopt, could be confusing in the future
-    return render_template('putfora.html')
+@app.route("/putfora") #the fact that this is adoption and the 
+def putfora():         #one below is adopt, could be confusing in the future
+    return render_template('putfora.html', title= "Put Animal Up for Adoption")
 
 @app.route("/adopt")
 def adopt():
-    return render_template('adopt.html')
+    return render_template('adopt.html', title= "Adopt/Purchase an Animal")
 
 @app.route("/messages")
 def messages():
-    return render_template('messages.html')
+    return render_template('messages.html', title= "Messages")
